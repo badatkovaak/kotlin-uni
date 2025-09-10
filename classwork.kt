b65075023e0b564f00560cfe23e3917c9db3ef2f
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2025-09-10 16:04:43 +0300

    Create classwork.kt

---
+++ classwork.kt
@@ -0,0 +1,59 @@
+
+typealias Tower = List<MutableList<Int>>
+    
+fun print_tower(tower: Tower) {
+    for(i in 0..2){
+        print("$i:")
+        for(x in tower[i])
+        	print(" $x ")
+            
+        println()
+    }
+}
+
+fun move(tower: Tower, origin: Int, dest: Int){
+    val last = tower[origin][tower[origin].size - 1]
+    tower[origin].removeAt(tower[origin].size - 1)
+    tower[dest].add(last)
+}
+
+fun solve_rec(tower: Tower,n: Int, origin: Int, dest: Int) {
+	if (n == 1){
+    	move(tower,origin, dest)
+        return
+    }
+    
+    solve_rec(tower, n-1, origin, 3 - dest - origin)
+    move(tower, origin, dest)
+    solve_rec(tower, n-1, 3 - dest - origin, dest)
+}
+
+data class myInteger(var v: Int)
+
+// fun max1(vararg x: Int): myInteger{
+//     val curr_max: myInteger = myInteger(-10000000)
+    
+//     fun max_inner(arr: IntArray, i: Int, curr_max: myInteger): myInteger {
+//         if (i >= arr.size)
+//         	return curr_max
+        
+//         if (curr_max.v < arr[i])
+//         	curr_max.v = arr[i]
+        
+//         return max_inner(arr, i+1, curr_max)
+//     }
+    
+//     return max_inner(x, 0, curr_max)
+// }
+
+fun max(vararg x: Int) = x.fold(-100000000){l: Int, r:Int -> if (r > l) r else l}
+
+
+fun main() {
+// 	println(max(1,2,3,5,7,4))
+    val tower: Tower = listOf(mutableListOf(4,3,2,1), mutableListOf(), mutableListOf())
+    print_tower(tower)
+    solve_rec(tower, 4, 0, 1)
+    println()
+    print_tower(tower)
+}
