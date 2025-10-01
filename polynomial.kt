9f9633ba53d9f89dcf0686fdc2d05db6bbc3bd5f
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2025-10-01 16:59:16 +0300

    Update polynomial.kt

--- polynomial.kt
+++ polynomial.kt
@@ -3,7 +3,7 @@ fun myzip(l1: List<Double>, l2: List<Double>): List<Pair<Double, Double>> {
     val list1: List<Double>
     
     if (l1.size != l2.size) {
-    	if (l1.size <= l2.size){
+    	if (l1.size < l2.size){
             list1 = List(l2.size, {if (it < l1.size) l1[it] else 0.0})
             list2 = l2
         } else {
@@ -30,17 +30,22 @@ fun f(l1: List<Double>, l2: List<Double>): List<Double> {
     return result
 }
 
+fun removeZeros(coeffs: List<Double>) : List<Double> {
+    var flag = false
+	return coeffs.reversed().filter{if (it != 0.0) flag = true ; it != 0.0 || flag}.reversed()
+}
+
 class Polynomial(val coeffs: List<Double>) {
     var degree: Int = this.coeffs.size - 1
     	get() = this.coeffs.size - 1
     
     constructor() :this(listOf(0.0)) {}
     
-    operator fun plus(p: Polynomial) = Polynomial(myzip(this.coeffs, p.coeffs).map{x: Pair<Double,Double> -> x.first + x.second})
+    operator fun plus(p: Polynomial) = Polynomial(removeZeros(myzip(this.coeffs, p.coeffs).map{it.first + it.second}))
     
-    operator fun times(c: Double) = Polynomial(this.coeffs.map{x: Double -> c*x})
+    operator fun times(c: Double) = Polynomial(this.coeffs.map{c*it})
     
-    operator fun times(p: Polynomial) = Polynomial(f(this.coeffs, p.coeffs))
+    operator fun times(p: Polynomial) = Polynomial(removeZeros(f(this.coeffs, p.coeffs)))
     
     
     override fun toString(): String {
@@ -56,16 +61,43 @@ class Polynomial(val coeffs: List<Double>) {
     }
 }
 
-fun main() {
-    val p1 = Polynomial(listOf(0.0, 0.0, 1.0))
-    val p2 = Polynomial(listOf(3.0, 2.0, 4.0, 5.0))
-    val p3 = Polynomial(listOf(2.0, 1.0))
+typealias P = Polynomial
+
+fun createBasisLagrangePolynomial(points: List<Pair<Double,Double>>, j: Int): Polynomial {
+   	var result = Polynomial(listOf(1.0))
     
-    println(p1)
-    println(p2)
-    println(p1 + p2)
-    println(p1 * 3.0)
-    println(p1)
-    println(p3)
-    println(p1 * p3)
+    for (i in 0..points.size-1) {
+        if (i == j) continue
+        result = result * P(listOf(-points[i].first, 1.0)) * (1/(points[j].first - points[i].first)) 
+    }
+    
+    return result
+}
+
+fun createBasis(points: List<Pair<Double,Double>>, j: Int) = 
+    points.withIndex()
+    	.fold(P(listOf(1.0)))
+		{l, r -> if (r.index == j) l else l*P(listOf(-r.value.first, 1.0))*(1/(points[j].first - r.value.first))}
+
+fun createLagrange(points: List<Pair<Double, Double>>) = points.withIndex().fold(P()){l, r -> l + createBasis(points, r.index)*r.value.second }
+        
+fun main() {
+    val p1 = P(listOf(0.0, 0.0, 1.0, -5.0))
+    val p2 = P(listOf(3.0, 2.0, 4.0, 5.0))
+    val p3 = P(listOf(2.0, 1.0))
+    val l1 = listOf(2.0, 0.0, 1.0, 0.0)
+    val l2 = listOf(Pair(1.0, 1.0), Pair(2.0, 4.0), Pair(3.0, 9.0))
+    
+//     println(p1)
+//     println(p2)
+//     println(p1 + p2)
+//     println(p1 * 3.0)
+//     println(p1)
+//     println(p3)
+//     println(p1 * p3)
+//     println(removeZeros(l))
+//     println(createBasisLagrangePolynomial(l2, 0))
+//     println(createBasis(l2, 2))
+    println(createLagrange(l2))
+//     println(l1.withIndex().map({println(it); 1.0}))
 }
